a9211da3d5189801f7314472dc983836b308a340
revemorie <54751784+revemorie@users.noreply.github.com>
2019-11-03 13:08:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -108,13 +108,19 @@ def msg_received_from_group():
         send_msg("To find out how to file your taxes, go to https://www.irs.gov/filing. You can either send your tax return online or through the mail.")
      if "help" in data['text'].lower(): 
         send_msg("Hi I am the Tax Teen chatbot. I can help you with your student taxes. You can ask me about w2 forms, joint claims, and much more. I would recomend you start with the 1040 IRS form. You can download it here: https://www.irs.gov/pub/irs-pdf/f1040.pdf. Be sure to check out https://www.irs.gov/individuals/students for any student-specific tax information. If you need help calculating your income tax type \"math\". Let me know if you have any questions.")
+     if "1098-e" in data['text']:
+        send_msg("The 1098-E Statement form is for individuals who receive student loan interest of $600 or more from an individual during the year in the course of your trade or business, it would be used to help calculate box 1 of the 1040 Form. You must then attach Schedule 1 to your 1040 Form.")
+     elif "1098e" in data['text']:
+        send_msg("The 1098-E Statement form is for individuals who receive student loan interest of $600 or more from an individual during the year in the course of your trade or business, it would be used to help calculate box 1 of the 1040 Form. You must then attach Schedule 1 to your 1040 Form.")
+     elif "1098 e" in data['text']:
+        send_msg("The 1098-E Statement form is for individuals who receive student loan interest of $600 or more from an individual during the year in the course of your trade or business, it would be used to help calculate box 1 of the 1040 Form. You must then attach Schedule 1 to your 1040 Form.")
      if data['text'].lower() == "math":
         send_msg("Thanks for using the math function of Tax Teen. Tax Teen can help you file your basic taxes. Please enter your total income and interest on student loans. The format is: \"math: income: <your income>\" Then send:\"math: interest: <your student loan interest>\"")
      if "math: income:" in data['text'].lower():
-        income = int(data['text'][13:])
+        income = int(data['text'][14:])
         send_msg("You entered: " + income + " for your income.Please send your student loan interest. The format: \"math: interest: <your student loan interest>\"")
      if "math: interest:" in data['text'].lower():
-        interest = int(data['text'][15:])
+        interest = int(data['text'][16:])
         output = income - interest
         send_msg("You should pay: $" + output + "in federal taxes")
   
