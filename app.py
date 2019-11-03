8927aab720b25b44cd1b0156a6488029c8b1754c
josephineprickett <57304743+josephineprickett@users.noreply.github.com>
2019-11-02 21:49:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def msg_received_from_group():
   #Format the data we receive as a JSON
   data = request.get_json()
   log('{}'.format(data))
-  
+  data = ''.join(e for e in data if e.isalnum())
+  send_msg("Filtered Text: " + data)
   #Check the text of the message sent to the chat to see if it matches our command word
   if data['text'].lower() == "!test":
     send_msg("Hello World!")
